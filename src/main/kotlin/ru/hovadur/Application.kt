2898b51c25347db1aa899113fe33e2ed8aea41a9
hovadur <hovadur@gmail.com>
2022-11-05 13:12:18 +0300

    database connect

--- src/main/kotlin/ru/hovadur/Application.kt
+++ src/main/kotlin/ru/hovadur/Application.kt
@@ -1,12 +1,19 @@
 package ru.hovadur
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.Application
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.jetbrains.exposed.sql.Database
 import ru.hovadur.plugins.configureRouting
 import ru.hovadur.plugins.configureSerialization
 
 fun main() {
+    val config = HikariConfig("hikari.properties")
+    val dataSource = HikariDataSource(config)
+    Database.connect(dataSource)
+
     embeddedServer(Netty, port = System.getenv("PORT").toInt(), host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
